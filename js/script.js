d98596bf68688d24982017c05cbe50b55687a32d
Lise Poirier <lise.p.poirier@gmail.com>
2016-11-18 14:32:23 +0100

    Mise en place de Jquerry

--- js/script.js
+++ js/script.js
@@ -22,9 +22,89 @@ $(document).ready(function () {
 
 function affichageprojet() {
     var element = document.getElementById("affichage-projet");
-        if(element.style.display == "block") {
-            element.style.display = "none";
-        } else {
-            element.style.display = "block";
-        }
+    if (element.style.display == "block") {
+        element.style.display = "none";
+    } else {
+        element.style.display = "block";
+    }
 }
+
+//hover presentation
+$('#present-1').ready(function () {
+    $('#present-1').mouseenter(function () {
+        $(this).css("width", "66.66%");
+        $(this).css("transition", "1s");
+        $('#hover-present-1').mouseenter(function () {
+            var div = $('#hover-present-1');
+            div.animate({
+                width: '100%',
+                opacity: '1'
+            }, "slow");
+        });
+    });
+    $('#present-1').mouseleave(function () {
+        $(this).css("width", "16.66666667%");
+           $('#hover-present-1').mouseleave(function () {
+            var div = $('#hover-present-1');
+            div.animate({
+                width: '100%',
+                opacity: '0',
+                transition: '0s'
+            }, "slow");
+        });
+    
+    });
+});
+
+//barre de progression
+$('#flamand').on('inview', function (event, isInView) {
+    if (isInView) {
+        $(this).css({
+            "width": "35vw",
+            "transition": "3s"
+        })
+    } else {
+        $(this).css({
+            "width": "5vw"
+        })
+    }
+});
+
+$('#elephant').on('inview', function (event, isInView) {
+    if (isInView) {
+        $(this).css({
+            "width": "32vw",
+            "transition": "3s"
+        })
+    } else {
+        $(this).css({
+            "width": "5vw"
+        })
+    }
+});
+
+$('#bear').on('inview', function (event, isInView) {
+    if (isInView) {
+        $(this).css({
+            "width": "28vw",
+            "transition": "3s"
+        })
+    } else {
+        $(this).css({
+            "width": "5vw"
+        })
+    }
+});
+
+$('#seal').on('inview', function (event, isInView) {
+    if (isInView) {
+        $(this).css({
+            "width": "20vw",
+            "transition": "3s"
+        })
+    } else {
+        $(this).css({
+            "width": "5vw"
+        })
+    }
+});
